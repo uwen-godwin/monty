9c936ea1baa81770d4218a3b6d2884d204d99b20
uwen-godwin <uwengodwin001@gmail.com>
2023-12-14 04:06:04 -0800

    push and pall

--- monty.h
+++ monty.h
@@ -1,22 +1,27 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define STACK_SIZE 100
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define STACK_SIZE 100
+#include <stdarg.h>
+#include <sys/types.h>
+#include <unistd.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
+ * @stack: Array to store elements
+ * @top: Index of the top element
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
+	int stack[STACK_SIZE];
+	int top;
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
@@ -34,19 +39,6 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-/**
- * struct stack - stack data structure
- * @stack: Array to store elements
- * @top: Index of the top element
- * Description:
- * Represents a Last-In, First-Out (LIFO)
- */
-typedef struct
-{
-	int stack[STACK_SIZE];
-	int top;
-} Stack;
 
 void push(Stack *stack, int value, int line_number);
 void pall(Stack *stack);
--- push_pall.c
+++ push_pall.c
@@ -1,64 +1,79 @@
 #include "monty.h"
-
-void push(Stack *stack, int value, int line_number) {
-    if (stack->top == STACK_SIZE - 1) {
-        fprintf(stderr, "L%d: Stack overflow\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    stack->top++;
-    stack->stack[stack->top] = value;
+/**
+ * push - Pushes a value onto the stack.
+ * @stack: Pointer to the stack.
+ * @value: Value to be pushed onto the stack.
+ * @line_number: Line number in the Monty file.
+ */
+void push(Stack *stack, int value, int line_number)
+{
+	if (stack->top == STACK_SIZE - 1)
+	{
+		fprintf(stderr, "L%d: Stack overflow\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack->top++;
+	stack->stack[stack->top] = value;
 }
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Pointer to the stack.
+ */
+void pall(Stack *stack)
+{
+	int i;
 
-void pall(Stack *stack) {
-    int i;
-    for (i = stack->top; i >= 0; i--) {
-        printf("%d\n", stack->stack[i]);
-    }
+	for (i = stack->top; i >= 0; i--)
+		printf("%d\n", stack->stack[i]);
 }
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	Stack stack;
+	char line[100], opcode[10];
+	int line_number = 0;
+	int value;
 
-int main(int argc, char *argv[]) {
-    FILE *file;
-    Stack stack;
-    char line[100];
-    int line_number = 0;
-    char opcode[10];
-    int value;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
-
-    stack.top = -1;
-
-    while (fgets(line, sizeof(line), file) != NULL) {
-        line_number++;
-
-        line[strcspn(line, "\n")] = '\0';
-
-        if (sscanf(line, "%s %d", opcode, &value) == 2) {
-            if (strcmp(opcode, "push") == 0) {
-                push(&stack, value, line_number);
-            } else {
-                fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
-                return EXIT_FAILURE;
-            }
-        } else if (strcmp(line, "pall") == 0) {
-            pall(&stack);
-        } else {
-            fprintf(stderr, "L%d: Invalid instruction: %s\n", line_number, line);
-            return EXIT_FAILURE;
-        }
-    }
-
-    fclose(file);
-
-    return EXIT_SUCCESS;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return (EXIT_FAILURE);
+	}
+	stack.top = -1;
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line_number++;
+		line[strcspn(line, "\n")] = '\0';
+		if (sscanf(line, "%s %d", opcode, &value) == 2)
+		{
+			if (strcmp(opcode, "push") == 0)
+				push(&stack, value, line_number);
+			else
+			{
+				fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
+				return (EXIT_FAILURE);
+			}
+		}
+		else if (strcmp(line, "pall") == 0)
+			pall(&stack);
+		else
+		{
+			fprintf(stderr, "L%d: Invalid instruction: %s\n", line_number, line);
+			return (EXIT_FAILURE);
+		}
+	}
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
