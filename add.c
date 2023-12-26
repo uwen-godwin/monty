502ddd282a6f8a1d52a75d50d6491a571da9c5c8
uwen-godwin <uwengodwin001@gmail.com>
2023-12-25 18:04:09 -0800

    updated

--- add.c
+++ add.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 
 /**
- * add - Adds the top two elements of the stack
- * @stack: Pointer to the top of the stack
- * @line_number: Line number in the Monty bytecode file
+ * add - Adds the top two elements of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number in the Monty file where the opcode was found.
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (!stack || !*stack || !(*stack)->next)
-	{
-		printf(stderr, "L%u: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	(*stack)->next->n += (*stack)->n;
-	pop(stack, line_number);
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n += (*stack)->n;
+    pop(stack, line_number);
 }
