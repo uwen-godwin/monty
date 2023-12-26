60e4c9a8ea98c1de2adfea81ee4dd862e0c39fb9
uwen-godwin <uwengodwin001@gmail.com>
2023-12-25 18:29:51 -0800

    The opcode swap swaps the top two elements of the stack.

--- swap.c
+++ swap.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * swap - Swaps the top two elements of the stack
  * @stack: Pointer to the top of the stack
@@ -6,15 +7,14 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    int temp;
+	int temp;
 
-    if (!stack || !*stack || !(*stack)->next)
-    {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
